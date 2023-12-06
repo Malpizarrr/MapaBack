a84a8e5b57f23792ec77fd0a682e935401e8d9d5
malpz <mauricio.alpizar.alpizar@est.una.ac.cr>
2023-12-05 22:14:46 -0600

    a

--- src/main/Front/front/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import { h, render } from 'preact';
-import MapaYRutas from './MapaYRutas';
-
-render(<MapaYRutas />, document.getElementById('app'));
-
-
-// Si necesitas ejecutar algo más antes de renderizar, asegúrate de que no esté fallando.
